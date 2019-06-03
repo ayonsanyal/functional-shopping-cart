0458da76a274a891fa3640a4819560f59574a137
ayonsanyal <ayon.sanyal@gmail.com>
2019-06-04 00:21:53 +0200

    Added customized error message for Shopping Cart module

---
+++ src/main/scala/api/shoppingcart/entities/ShoppingCartErrorMessages.scala
@@ -0,0 +1,7 @@
+package api.shoppingcart.entities
+
+import api.common.entities.ErrorMessages
+
+trait ShoppingCartErrorMessages extends ErrorMessages {
+  override val RESULT_NOT_FOUND: String = "Shopping cart is empty,so cannot checkout"
+}
